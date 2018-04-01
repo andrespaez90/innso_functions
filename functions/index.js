fbf431e83293b3e79028aa1d66878e11a69312dd
Andrés Páez <andres.paez@innso.co>
2018-04-01 16:03:12 -0500

    improve function that calculate total by month and year including paid taxes, received taxes and expenses.

--- functions/index.js
+++ functions/index.js
@@ -30,7 +30,7 @@ exports.addBillTrigger = functions.database
         const bills = event.data.val();
         console.log('addBillTrigger', bills);
 
-        if(bills != null){;
+        if (bills != null) {
             Object.keys(bills).map(function (key, index) {
                 const bill = bills[key];
                 revenue += bill.total;
@@ -39,30 +39,68 @@ exports.addBillTrigger = functions.database
         }
 
         let year = event.params.year;
-        let month = event.params.month
+        let month = event.params.month;
         let dbRef = admin.database().ref(`finance/${year}/summary/${month}`);
         dbRef.child('revenue').set(revenue);
-        dbRef.child('taxes').set(taxes);
+        dbRef.child('received_taxes').set(taxes);
 
         return false
     });
 
+exports.addExpenseTrigger = functions.database
+    .ref("expenses/{year}/{month}")
+    .onWrite(event => {
 
-exports.onRevenueMonthChange = functions.database
-    .ref("finance/{year}/summary")
-    .onWrite( event => {
+        let expense = 0;
+        let taxes = 0;
+        const bills = event.data.val();
+        console.log('addExpenseTrigger', bills);
 
-        let totalRevenue = 0;
-        const summaries = event.data.val();
-        console.log('onValuesMonthChange', summaries);
-        if(summaries != null){
-            Object.keys(summaries).map(function (key, index) {
-                totalRevenue += summaries[key].revenue;
+        if (bills != null) {
+            Object.keys(bills).map(function (key, index) {
+                const bill = bills[key];
+                expense += bill.total;
+                taxes += bill.taxes;
             });
         }
-        return event.data.ref.parent.child('total_revenue').set(totalRevenue);
+
+        let year = event.params.year;
+        let month = event.params.month;
+        let dbRef = admin.database().ref(`finance/${year}/summary/${month}`);
+        dbRef.child('expenses').set(expense);
+        dbRef.child('paid_taxes').set(taxes);
+        return false
     });
 
+
+exports.onMonthValuesChange = functions.database
+    .ref("finance/{year}/summary")
+    .onWrite(event => {
+
+        let totalRevenue = 0;
+        let totalExpenditure = 0;
+        let totalPaidTaxes = 0;
+        let totalReceivedTaxes = 0;
+
+        const summaries = event.data.val();
+        console.log('onMonthValuesChange', summaries);
+        if (summaries != null) {
+            Object.keys(summaries).map(function (key, index) {
+                const month = summaries[key];
+                totalRevenue += month.revenue != null ? month.revenue : 0;
+                totalExpenditure += month.expenses != null ? month.expenses : 0;
+                totalPaidTaxes += month.paid_taxes != null ? month.paid_taxes : 0;
+                totalReceivedTaxes += month.received_taxes != null ? month.received_taxes : 0;
+            });
+        }
+
+        let parent = event.data.ref.parent;
+        parent.child('total_revenue').set(totalRevenue);
+        parent.child('total_expenditure').set(totalExpenditure);
+        parent.child('total_paid_taxes').set(totalPaidTaxes);
+        parent.child('total_received_taxes').set(totalReceivedTaxes);
+        return true;
+    });
 
 function hasAuthorization(request) {
     const authorization = request.get('authorization');
